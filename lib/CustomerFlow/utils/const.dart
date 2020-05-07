c12e9d64d214f894cd601ff9f1c58d4c07b862a6
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2020-05-07 20:23:07 +0530

    Update const.dart

--- lib/CustomerFlow/utils/const.dart
+++ lib/CustomerFlow/utils/const.dart
@@ -5,17 +5,20 @@ class CustomTextField extends StatelessWidget {
       {this.icon,
         this.hint,
         this.obsecure = false,
+        this.fillcolor=true,
         this.validator,
         this.onSaved});
   final FormFieldSetter<String> onSaved;
   final Icon icon;
   final String hint;
   final bool obsecure;
+  final bool fillcolor;
   final FormFieldValidator<String> validator;
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left: 20, right: 20),
+      height: 50,
+      padding: EdgeInsets.only(left: 30, right: 30),
       child: TextFormField(
         onSaved: onSaved,
         validator: validator,
@@ -25,19 +28,19 @@ class CustomTextField extends StatelessWidget {
           fontSize: 20,
         ),
         decoration: InputDecoration(
-          fillColor: Colors.white,
-            filled: true,
+          fillColor: Color.fromRGBO(235, 235, 235, 1),
+            filled: fillcolor,
             hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             hintText: hint,
             enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.horizontal(right: Radius.circular(20),left: Radius.circular(20)),
+              borderRadius: BorderRadius.horizontal(right: Radius.circular(5),left: Radius.circular(5)),
               borderSide: BorderSide(
                 color: Colors.white,
                 width: 2,
               ),
             ),
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.horizontal(right: Radius.circular(20),left: Radius.circular(20)),
+              borderRadius: BorderRadius.horizontal(right: Radius.circular(5),left: Radius.circular(5)),
               borderSide: BorderSide(
                 color: Theme.of(context).primaryColor,
                 width: 3,
@@ -48,3 +51,38 @@ class CustomTextField extends StatelessWidget {
     );
   }
 }
+
+const kMessageTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  hintText: 'Type your message here...',
+  border: InputBorder.none,
+);
+
+const kMessageContainerDecoration = BoxDecoration(
+  border: Border(
+    top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+  ),
+);
+const kTextFieldDecoration =InputDecoration(
+  hintText: 'Enter your password.',
+  hintStyle: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.black87
+  ),
+  prefixIcon:null,
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: Colors.black, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: Colors.white, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
