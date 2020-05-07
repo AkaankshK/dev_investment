17f1944336eefacf469e3f0f22fa6a60c310a952
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2020-05-07 20:20:54 +0530

    Add files via upload

---
+++ lib/Employee/screens/Cash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class cash extends StatefulWidget {
+  @override
+  _cashState createState() => _cashState();
+}
+
+class _cashState extends State<cash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
